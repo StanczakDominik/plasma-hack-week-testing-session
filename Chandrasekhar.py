9f37b1a790f05217b66ab054a02d0d5c9f797ff1
Dominik Stańczak <stanczakdominik@gmail.com>
2021-07-01 20:06:04 +0200

    Add special case for x = 0

--- Chandrasekhar.py
+++ Chandrasekhar.py
@@ -3,5 +3,7 @@ import numpy as np
 
 
 def G(x):
+    if np.isclose(x, 0):
+        return 0
     erf_derivative = 2 * np.exp(-(x ** 2)) / np.sqrt(np.pi)
     return (erf(x) / x ** 2 - erf_derivative / x) / 2
