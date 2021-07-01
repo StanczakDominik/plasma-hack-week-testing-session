ac068395d965b57d6d625a27cb7d3d6a8104f43b
Dominik Stańczak <stanczakdominik@gmail.com>
2021-07-01 20:33:25 +0200

    This is a serious change.

--- Chandrasekhar.py
+++ Chandrasekhar.py
@@ -3,9 +3,4 @@ import numpy as np
 
 
 def G(x):
-    x = np.asarray(x)
-    with np.errstate(divide='ignore', invalid='ignore'):
-        erf_derivative = 2 * np.exp(-x**2) / np.sqrt(np.pi)
-        output = (erf(x) / x **2 - erf_derivative / x) / 2
-    output = np.where(x == 0, 0, output)
-    return output
+    return "Muffins!"
