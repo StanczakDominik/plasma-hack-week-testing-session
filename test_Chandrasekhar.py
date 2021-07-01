18ae85d758107140caa12e4a92d7d8cb807766c3
Dominik Stańczak <stanczakdominik@gmail.com>
2021-07-01 20:32:06 +0200

    Add simple regression test

--- test_Chandrasekhar.py
+++ test_Chandrasekhar.py
@@ -1,5 +1,6 @@
 from Chandrasekhar import G
 import numpy as np
+from numpy.testing import assert_allclose
 
 
 def test_known_values_Chandrasekhar_G():
@@ -13,8 +14,35 @@ def test_known_values_Chandrasekhar_G():
     assert np.isclose(G(0), 0)
     assert isinstance(G(0), np.ndarray)
 
+
 def test_known_values_as_numpy_array_Chandrasekhar_G():
     x = np.array([1e30, 1e-16, 0])
     assert np.allclose(G(x), 0)
     assert isinstance(G(x), np.ndarray)
     assert G(x).size == 3
+
+
+def test_regression_G():
+    x = np.linspace(-5, 5, 15)
+    y = G(x)
+    stored_values = np.array(
+        [
+            -0.02,
+            -0.02722222,
+            -0.03919953,
+            -0.06119047,
+            -0.10595478,
+            -0.18306813,
+            -0.19961233,
+            0.0,
+            0.19961233,
+            0.18306813,
+            0.10595478,
+            0.06119047,
+            0.03919953,
+            0.02722222,
+            0.02,
+        ]
+    )
+
+    assert_allclose(y, stored_values)
