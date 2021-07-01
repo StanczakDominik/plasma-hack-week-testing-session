643000b0628eb209d26066cf2f388a023641d2ac
Dominik Stańczak <stanczakdominik@gmail.com>
2021-07-01 20:11:54 +0200

    Add some basic tests for input/output types

--- test_Chandrasekhar.py
+++ test_Chandrasekhar.py
@@ -8,9 +8,13 @@ def test_known_values_Chandrasekhar_G():
 
     small_x = 1e-16
     assert np.isclose(G(small_x), 0)
+    assert isinstance(G(small_x), float)
 
     assert np.isclose(G(0), 0)
+    assert isinstance(G(0), float)
 
 def test_known_values_as_numpy_array_Chandrasekhar_G():
     x = np.array([1e30, 1e-16, 0])
     assert np.allclose(G(x), 0)
+    assert isinstance(G(x), np.ndarray)
+    assert G(x).size == 3
