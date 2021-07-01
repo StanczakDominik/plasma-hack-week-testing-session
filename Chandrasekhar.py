e63895cbfd8e0f3a92cec006bf69f5ad344bf26c
Dominik Stańczak <stanczakdominik@gmail.com>
2021-07-01 20:13:29 +0200

    Add np.errstate block to ignore warnings

--- Chandrasekhar.py
+++ Chandrasekhar.py
@@ -4,7 +4,8 @@ import numpy as np
 
 def G(x):
     x = np.asarray(x)
-    erf_derivative = 2 * np.exp(-x**2) / np.sqrt(np.pi)
-    output = (erf(x) / x **2 - erf_derivative / x) / 2
+    with np.errstate(divide='ignore', invalid='ignore'):
+        erf_derivative = 2 * np.exp(-x**2) / np.sqrt(np.pi)
+        output = (erf(x) / x **2 - erf_derivative / x) / 2
     output = np.where(x == 0, 0, output)
     return output
