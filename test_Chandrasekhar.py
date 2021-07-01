b9c8a8f9cce535092495b6052a68eb0616137751
Dominik Stańczak <stanczakdominik@gmail.com>
2021-07-01 20:07:35 +0200

    Add test for numpy array input

--- test_Chandrasekhar.py
+++ test_Chandrasekhar.py
@@ -10,3 +10,7 @@ def test_known_values_Chandrasekhar_G():
     assert np.isclose(G(small_x), 0)
 
     assert np.isclose(G(0), 0)
+
+def test_known_values_as_numpy_array_Chandrasekhar_G():
+    x = np.array([1e30, 1e-16, 0])
+    assert np.allclose(G(x), 0)
